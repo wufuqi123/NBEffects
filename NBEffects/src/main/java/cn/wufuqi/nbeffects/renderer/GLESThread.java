8313a39c99de83f12e26de644e84c76c54462a80
吴富其 <1090323359@qq.com>
2022-12-06 16:34:20 +0800

    提交一些去掉中文

--- NBEffects/src/main/java/cn/wufuqi/nbeffects/renderer/GLESThread.java
+++ NBEffects/src/main/java/cn/wufuqi/nbeffects/renderer/GLESThread.java
@@ -50,11 +50,9 @@ public class GLESThread extends Thread {
                 }
             }
 
-            mPendingThreadAider.runPendings();// 执行未执行的，或要执行的事件。（后期可以开放以便模仿GLSurfaceView的queueEvent(Runnable r)）
-            mRenderer.onDrawFrame(null);// 绘制
-            // 一帧完成之后，调用eglSwapBuffers(EGLDisplay dpy, EGLContext ctx)来显示
-            mEgl.eglSwapBuffers(mEglDisplay, mEglSurface);// 这一句不能少啊，少了就GG了，一片空白
-            // 1.凡是onPause都要停止，2.如果是onResume的状态，如果是循环刷新则会继续下一次循环，否则会暂停等待调用requestRender()
+            mPendingThreadAider.runPendings();
+            mRenderer.onDrawFrame(null);
+            mEgl.eglSwapBuffers(mEglDisplay, mEglSurface);
             if (mIsPaused) {
                 pauseWhile();
             } else if (mRendererMode == NBTextureView.RENDERMODE_WHEN_DIRTY) {
@@ -75,36 +73,32 @@ public class GLESThread extends Thread {
         }
 
         int[] version = new int[2];
-        if (!mEgl.eglInitialize(mEglDisplay, version)) {// //version中存放EGL 版本号，int[0]为主版本号，int[1]为子版本号
+        if (!mEgl.eglInitialize(mEglDisplay, version)) {
             throw new RuntimeException("eglInitialize failed : " + GLUtils.getEGLErrorString(mEgl.eglGetError()));
         }
 
-        // 构造需要的特性列表
         int[] configAttribs = { //
-                EGL10.EGL_BUFFER_SIZE, 32,//
-                EGL10.EGL_ALPHA_SIZE, 8, // 指定Alpha大小，以下四项实际上指定了像素格式
-                EGL10.EGL_BLUE_SIZE, 8, // 指定B大小
-                EGL10.EGL_GREEN_SIZE, 8,// 指定G大小
-                EGL10.EGL_RED_SIZE, 8,// 指定RGB中的R大小（bits）
-                EGL10.EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,// 指定渲染api类别,这里或者是硬编码的4，或者是EGL14.EGL_OPENGL_ES2_BIT
-                EGL10.EGL_SURFACE_TYPE, EGL10.EGL_WINDOW_BIT, EGL10.EGL_NONE// 总是以EGL10.EGL_NONE结尾
+                EGL10.EGL_BUFFER_SIZE, 32,
+                EGL10.EGL_ALPHA_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_SURFACE_TYPE, EGL10.EGL_WINDOW_BIT, EGL10.EGL_NONE
         };
 
         int[] numConfigs = new int[1];
         EGLConfig[] configs = new EGLConfig[1];
-        // eglChooseConfig(display, attributes, configs, num, configNum);
-        // 用于获取满足attributes的所有config，参数1、2其意明显，参数3用于存放输出的configs，参数4指定最多输出多少个config，参数5由EGL系统写入，表明满足attributes的config一共有多少个
-        if (!mEgl.eglChooseConfig(mEglDisplay, configAttribs, configs, 1, numConfigs)) {// 获取所有满足attributes的configs,并选择一个
+        if (!mEgl.eglChooseConfig(mEglDisplay, configAttribs, configs, 1, numConfigs)) {
             throw new RuntimeException("eglChooseConfig failed : " + GLUtils.getEGLErrorString(mEgl.eglGetError()));
         }
 
-        int[] contextAttribs = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};// attrib_list,目前可用属性只有EGL_CONTEXT_CLIENT_VERSION, 1代表OpenGL ES 1.x,
-        // 2代表2.0。同样在Android4.2之前，没有EGL_CONTEXT_CLIENT_VERSION这个属性，只能使用硬编码0x3098代替
-        mEglContext = mEgl.eglCreateContext(mEglDisplay, configs[0], EGL10.EGL_NO_CONTEXT, // share_context,是否有context共享，共享的contxt之间亦共享所有数据。EGL_NO_CONTEXT代表不共享
-                contextAttribs);// 创建context
-        mEglSurface = mEgl.eglCreateWindowSurface(mEglDisplay, configs[0], mSurfaceTexture,// 负责对Android Surface的管理
-                null// Surface属性
-        );// 获取显存，create a new EGL window surface
+        int[] contextAttribs = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};
+        mEglContext = mEgl.eglCreateContext(mEglDisplay, configs[0], EGL10.EGL_NO_CONTEXT,
+                contextAttribs);
+        mEglSurface = mEgl.eglCreateWindowSurface(mEglDisplay, configs[0], mSurfaceTexture,
+                null
+        );
         if (mEglSurface == EGL10.EGL_NO_SURFACE || mEglContext == EGL10.EGL_NO_CONTEXT) {
             int error = mEgl.eglGetError();
             if (error == EGL10.EGL_BAD_NATIVE_WINDOW) {
@@ -113,7 +107,7 @@ public class GLESThread extends Thread {
             throw new RuntimeException("eglCreateWindowSurface failed : " + GLUtils.getEGLErrorString(mEgl.eglGetError()));
         }
 
-        if (!mEgl.eglMakeCurrent(mEglDisplay, mEglSurface, mEglSurface, mEglContext)) {// 设置为当前的渲染环境
+        if (!mEgl.eglMakeCurrent(mEglDisplay, mEglSurface, mEglSurface, mEglContext)) {
             throw new RuntimeException("eglMakeCurrent failed : " + GLUtils.getEGLErrorString(mEgl.eglGetError()));
         }
 //        mGL = mEglContext.getGL();
@@ -148,7 +142,7 @@ public class GLESThread extends Thread {
     }
 
     public void onSurfaceChanged(final int width, final int height) {
-        mPendingThreadAider.addToPending(new Runnable() {// 在GL线程中执行
+        mPendingThreadAider.addToPending(new Runnable() {
 
             @Override
             public void run() {
